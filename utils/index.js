0540c5f0b4a5c6becc97136e373eae9479a64aee
RussoSk <117772437+RussoSk@users.noreply.github.com>
2023-10-10 18:39:49 +0300

    7.0
    hw-06

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,14 @@
 const HttpError = require("./httpError");
 const validateData = require("./validateData");
 const { requestError, addRequestError } = require("./requestError");
+const sendEmail = require("./sendEmail");
+const resizeAvatar = require("./resizeAvatar");
 
 module.exports = {
   HttpError,
   validateData,
   requestError,
   addRequestError,
+  resizeAvatar,
+  sendEmail
 };
